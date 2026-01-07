d34572c6b0505be0161bbc8624214233de252326
atg205 <hanup@mailbox.org>
2026-01-07 18:01:04 +0100

    first commit

---
+++ capture_preprocess.py
@@ -0,0 +1,18 @@
+Error!! does not exist: /home/atg205/Malware/iot23/mal/CTU-IoT-Malware-Capture-43-1/bro/conn.log.labeled
+Warning!! No files to merge: mirai
+Error!! does not exist: /home/atg205/Malware/iot23/mal/CTU-IoT-Malware-Capture-17-1/bro/conn.log.labeled
+Warning!! No files to merge: kenjiro
+Error!! does not exist: /home/atg205/Malware/iot23/mal/CTU-IoT-Malware-Capture-60-1/bro/conn.log.labeled
+Warning!! No files to merge: gafgyt
+Error!! does not exist: /home/atg205/Malware/iot23/mal/CTU-IoT-Malware-Capture-36-1/bro/conn.log.labeled
+Warning!! No files to merge: okiru
+Error!! does not exist: /home/atg205/Malware/iot23/mal/CTU-IoT-Malware-Capture-8-1/bro/conn.log.labeled
+Warning!! No files to merge: hakai
+Error!! does not exist: /home/atg205/Malware/iot23/mal/CTU-IoT-Malware-Capture-39-1/bro/conn.log.labeled
+Warning!! No files to merge: ircbot
+Error!! does not exist: /home/atg205/Malware/iot23/mal/CTU-IoT-Malware-Capture-9-1/bro/conn.log.labeled
+Warning!! No files to merge: linux.hajime
+Error!! does not exist: /home/atg205/Malware/iot23/mal/CTU-IoT-Malware-Capture-3-1/bro/conn.log.labeled
+Warning!! No files to merge: muhstik
+Error!! does not exist: /home/atg205/Malware/iot23/mal/CTU-IoT-Malware-Capture-1-1/bro/conn.log.labeled
+Warning!! No files to merge: hideandseek
